62ceed68798f8838a12b8f8c0d8add851081e25b
Teo Fumagalli <teo.fum@gmail.com>
2025-05-30 20:10:50 -0300

    tp5 2.1

--- tp5/src/main/java/com/itba/eda/Expression/ExpTree.java
+++ tp5/src/main/java/com/itba/eda/Expression/ExpTree.java
@@ -8,6 +8,10 @@ public class ExpTree implements ExpressionService {
     public ExpTree(String expr) {
         Scanner ls = new Scanner(expr).useDelimiter("\\s+");
         root = new Node(ls);
+
+        if (ls.hasNext())
+            throw new RuntimeException("Bad Expression");
+
         ls.close();
     }
 
@@ -16,15 +20,18 @@ public class ExpTree implements ExpressionService {
         private Node left, right;
 
         public Node(Scanner ls) {
-            buildExpression(ls);
-            if (ls.hasNext())
-                throw new RuntimeException("Bad Expression");
-        }
+            switch (ls.next()) {
+                case "(" -> {
+                    left = new Node(ls); // Parse left expression
+                    data = ls.next(); // Store operator
+                    right = new Node(ls); // Parse right expression
 
-        private void buildExpression(Scanner ls) {
-            data = null;
-            left = null;
-            right = null;
+                    // Expect closing parenthesis
+                    if (!ls.next().equals(")"))
+                        throw new RuntimeException("Expected )");
+                }
+                case String token -> data = token;
+            }
         }
     }
 }
